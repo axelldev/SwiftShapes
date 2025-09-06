983875a3096ab42b1ddb21af1aa875e26ed275aa
axelldev <axellsolisrubio@gmail.com>
2025-09-06 17:48:02 -0600

    Initial Commit

---
+++ SwiftShapes/SwiftShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShapesApp.swift
+//  SwiftShapes
+//
+//  Created by axell solis on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
